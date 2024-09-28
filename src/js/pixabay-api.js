ebf3e06b91347c3822c1e29dd2ca26189b70cc65
MariyaErmakova2023 <ermakova.mariya2017@gmail.com>
2024-09-28 18:29:32 +0300

    zerro

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,54 +1,57 @@
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '43997901-59e2ef8a5969ad3162d77619c';
+// const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '43997901-59e2ef8a5969ad3162d77619c';
 
-export const searchPhotos = query => {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+// export const searchPhotos = query => {
+//   const searchParams = new URLSearchParams({
+//     key: API_KEY,
+//     q: query,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: true,
+//   });
 
-  return fetch(`${BASE_URL}?${searchParams}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
-};
-export default getImage;
+//   return fetch(`${BASE_URL}?${searchParams}`).then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.statusText);
+//     }
+//     return response.json();
+//   });
+// };
+// export default getImage;
 
-function getImage(image) {
-  const API_KEY = '44666739-d0cf1ddb18f9d30146fa54081';
-  const BASE_URL = 'https://pixabay.com/api/';
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: image,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
-  });
-  console.log(searchParams.toString());
-  return fetch(`${BASE_URL}?${searchParams}`).then(resp => {
-    if (!resp.ok) {
-      throw new Error(resp.statusText);
-    }
-    return resp.json();
-  });
-}
-==========
-export function searchGalleryQuery(query) {
-    const URL = "https://pixabay.com/api/";
-    const API_KEY = "45091921-bafbe55c990439d7032dec8c8";
+// function getImage(image) {
+//   const API_KEY = '44666739-d0cf1ddb18f9d30146fa54081';
+//   const BASE_URL = 'https://pixabay.com/api/';
+//   const searchParams = new URLSearchParams({
+//     key: API_KEY,
+//     q: image,
+//     image_type: 'photo',
+//     orientation: 'horizontal',
+//     safesearch: 'true',
+//   });
+//   console.log(searchParams.toString());
+//   return fetch(`${BASE_URL}?${searchParams}`).then(resp => {
+//     if (!resp.ok) {
+//       throw new Error(resp.statusText);
+//     }
+//     return resp.json();
+//   });
+// }
+// ///==========
+// export function searchGalleryQuery(query) {
+//   const URL = 'https://pixabay.com/api/';
+//   const API_KEY = '45091921-bafbe55c990439d7032dec8c8';
 
-    return fetch(`${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-        })
-        .catch((error) => {
-        console.log(error);
-    })
+//   return fetch(
+//     `${URL}?key=${API_KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
+//   )
+//     .then(response => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+// }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,72 +1,75 @@
-import getImage from './pixabay-api';
+// import getImage from './pixabay-api';
 
-export default createMarcupGallery;
+// export default createMarcupGallery;
 
-function createMarcupGallery(images) {
-  return images
-    .map(
-      element =>
-        `<li class="gallery-item">
-          <div class="gallery-image-tumb">
-            <a class="gallery-link" href="${element.largeImageURL}">
-                <img class="gallery-image" src="${element.webformatURL}" alt="${element.tags}" />
-            </a>
-          </div>
-            <ul class="image-info">
-              <li class="image-info-item">
-                <h4 class="image-info-item-title">Likes</h4>
-                <p class="image-info-item-text">${element.likes}</p>
-              </li>
-              <li class="image-info-item">
-                <h4 class="image-info-item-title">Views</h4>
-                <p class="image-info-item-text">${element.views}</p>
-              </li>
-              <li class="image-info-item">
-                <h4 class="image-info-item-title">Comments</h4>
-                <p class="image-info-item-text">${element.comments}</p>
-              </li>
-              <li class="image-info-item">
-                <h4 class="image-info-item-title">Downloads</h4>
-                <p class="image-info-item-text">${element.downloads}</p>
-              </li>
-            </ul>
-        </li>`
-    )
-    .join('');
-}
+// function createMarkquitupGallery(images) {
+//   return images
+//     .map(
+//       element =>
+//         `<li class="gallery-item">
+//           <div class="gallery-image-tumb">
+//             <a class="gallery-link" href="${element.largeImageURL}">
+//                 <img class="gallery-image" src="${element.webformatURL}" alt="${element.tags}" />
+//             </a>
+//           </div>
+//             <ul class="image-info">
+//               <li class="image-info-item">
+//                 <h4 class="image-info-item-title">Likes</h4>
+//                 <p class="image-info-item-text">${element.likes}</p>
+//               </li>
+//               <li class="image-info-item">
+//                 <h4 class="image-info-item-title">Views</h4>
+//                 <p class="image-info-item-text">${element.views}</p>
+//               </li>
+//               <li class="image-info-item">
+//                 <h4 class="image-info-item-title">Comments</h4>
+//                 <p class="image-info-item-text">${element.comments}</p>
+//               </li>
+//               <li class="image-info-item">
+//                 <h4 class="image-info-item-title">Downloads</h4>
+//                 <p class="image-info-item-text">${element.downloads}</p>
+//               </li>
+//             </ul>
+//         </li>`
+//     )
+//     .join('');
+// }
 
+// //========
+// // import SimpleLightbox from 'simplelightbox';
+// // import 'simplelightbox/dist/simple-lightbox.min.css';
 
-//========
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// export function createImages(data) {
+//   const lightbox = new SimpleLightbox('.gallery-list a', {
+//     captions: true,
+//     captionsData: 'alt',
+//     captionsDelay: 250,
+//   });
 
-export function createImages(data) {
-    const lightbox = new SimpleLightbox('.gallery-list a', {
-        captions: true,
-        captionsData: 'alt',
-        captionsDelay: 250
-    });
+//   const galleryList = document.querySelector('.gallery-list');
 
-    const galleryList = document.querySelector('.gallery-list');
+//   let images = data.hits
+//     .map(
+//       image =>
+//         `<div class="image-wrapper">
+//     <a href="${image.largeImageURL}">
+//     <img class="gallery-img" src="${image.webformatURL}" alt="${image.tags}"></img>
+//     </a>
+//     <div class="text-wrapper">
+//     <div class="text-item"><h5 class="text-header">Likes</h5><p class="text-paragraph">${image.likes}</p></div>
+//     <div class="text-item"><h5 class="text-header">Views</h5><p class="text-paragraph">${image.views}</p></div>
+//     <div class="text-item"><h5 class="text-header">Comments</h5><p class="text-paragraph">${image.comments}</p></div>
+//     <div class="text-item"><h5 class="text-header">Downloads</h5><p class="text-paragraph">${image.downloads}</p></div>
+//     </div>
+//     </div>`
+//     )
+//     .join('');
 
-    let images = data.hits.map((image) =>
-        `<div class="image-wrapper">
-    <a href="${image.largeImageURL}">
-    <img class="gallery-img" src="${image.webformatURL}" alt="${image.tags}"></img>
-    </a>
-    <div class="text-wrapper">
-    <div class="text-item"><h5 class="text-header">Likes</h5><p class="text-paragraph">${image.likes}</p></div>
-    <div class="text-item"><h5 class="text-header">Views</h5><p class="text-paragraph">${image.views}</p></div>
-    <div class="text-item"><h5 class="text-header">Comments</h5><p class="text-paragraph">${image.comments}</p></div>
-    <div class="text-item"><h5 class="text-header">Downloads</h5><p class="text-paragraph">${image.downloads}</p></div>
-    </div>
-    </div>`).join("");
-        
-    galleryList.insertAdjacentHTML('beforeend', images);
-    lightbox.refresh();
-}
+//   galleryList.insertAdjacentHTML('beforeend', images);
+//   lightbox.refresh();
+// }
 
-export function clearImages() {
-    const galleryList = document.querySelector('.gallery-list');
-    galleryList.innerHTML = "";
-}
+// export function clearImages() {
+//   const galleryList = document.querySelector('.gallery-list');
+//   galleryList.innerHTML = '';
+// }
